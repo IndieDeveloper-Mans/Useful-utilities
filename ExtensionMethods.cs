5f08c8c0d9f61e263308700823561a487e321eb6
Mansur <56151168+IndieDeveloper-Mans@users.noreply.github.com>
2022-02-04 13:40:20 +0300

    Update ExtensionMethods.cs

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -2,15 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//It is common to create a class to contain all of your
-//extension methods. This class must be static.
 public static class ExtensionMethods
 {
-    //Even though they are used like normal methods, extension
-    //methods must be declared static. Notice that the first
-    //parameter has the 'this' keyword followed by a Transform
-    //variable. This variable denotes which class the extension
-    //method becomes a part of.
     public static void ResetTransformation(this Transform trans)
     {
         trans.localPosition = Vector3.zero;
